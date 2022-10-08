91abd888f4443827269f1777d76b3655b3fc8253
Yukki 'o <43243572+Nagami-Yukki@users.noreply.github.com>
2022-10-08 08:47:56 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# JLFork
+[![Download](https://jitpack.io/v/jagrosh/JLyrics.svg)](https://jitpack.io/#jagrosh/JLyrics/master-SNAPSHOT)
+[![Stars](https://img.shields.io/github/stars/jagrosh/JLyrics.svg)](https://github.com/jagrosh/JLyrics/stargazers)
+[![License](https://img.shields.io/github/license/jagrosh/JLyrics.svg)](https://github.com/jagrosh/JLyrics/blob/master/LICENSE)
+[![CodeFactor](https://www.codefactor.io/repository/github/jagrosh/jlyrics/badge)](https://www.codefactor.io/repository/github/jagrosh/jlyrics)
+[![Build status](https://ci.appveyor.com/api/projects/status/7gyee0nnox0y4756?svg=true)](https://ci.appveyor.com/project/jagrosh/jlyrics)
+
+# JLyrics  
+🎼 Expandable lyrics-scraping API for Java
+
+## Example
+```java
+LyricsClient client = new LyricsClient();
+Lyrics lyrics = client.getLyrics("smooth criminal").get();
+System.out.println(lyrics.getContent()); // As he came into the window ...
+```
+
+## Features
+  * Async or blocking (uses [CompletableFuture](https://docs.oracle.com/javase/8/docs/api/java/util/concurrent/CompletableFuture.html))
+  * Built-in caching to prevent duplicate scrapes
+  * Highly customizable (see [reference.conf](https://github.com/jagrosh/JLyrics/blob/master/src/main/resources/reference.conf) and [lightbend/config](https://github.com/lightbend/config))
+  * Easily expandable to many lyrics sites
+
+## Included Lyrics Sites
+  * A-Z Lyrics
+  * Genius
+  * MusixMatch
+  * [your contribution here]
+
+## Maven Setup
+```xml
+<repository>
+		  <id>jitpack.io</id>
+	   <url>https://jitpack.io</url>
+</repository>
+```
+
+```xml
+<dependency>
+	   <groupId>com.github.jagrosh</groupId>
+	   <artifactId>JLyrics</artifactId>
+	   <version>master-SNAPSHOT</version>
+</dependency>
+```
---
+++ src/test/java/com/jagrosh/jlyrics/LyricsTest.java
@@ -0,0 +1,146 @@
+/*
+ * Copyright 2018 John Grosh (john.a.grosh@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jagrosh.jlyrics;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import java.util.concurrent.ExecutionException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author John Grosh (john.a.grosh@gmail.com)
+ */
+public class LyricsTest
+{
+    @Test
+    public void configurationTest()
+    {
+        Config config = ConfigFactory.load();
+        assertNotNull(config);
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void invalidSource() throws InterruptedException, ExecutionException
+    {
+        LyricsClient client = new LyricsClient();
+        
+        Lyrics ignored = client.getLyrics("smooth criminal", "Source not in config anywhere").get();
+    }
+    
+    @Test
+    public void azlyricsTest() throws InterruptedException, ExecutionException
+    {
+        LyricsClient client = new LyricsClient("A-Z Lyrics");
+        
+        Lyrics lyrics = client.getLyrics("ellie goulding lights").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+        assertNotNull(lyrics.getAuthor());
+        assertNotNull(lyrics.getContent());
+        assertNotNull(lyrics.getSource());
+        
+        lyrics = client.getLyrics("jklsjdgv89y32hr9").get();
+        assertNull(lyrics);
+        
+        lyrics = client.getLyrics("smooth criminal").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+        assertNotNull(lyrics.getAuthor());
+        assertNotNull(lyrics.getContent());
+        assertNotNull(lyrics.getSource());
+    }
+    
+    @Test
+    public void geniusTest() throws InterruptedException, ExecutionException
+    {
+        LyricsClient client = new LyricsClient("Genius");
+        
+        Lyrics lyrics = client.getLyrics("ellie goulding lights").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+        assertNotNull(lyrics.getAuthor());
+        assertNotNull(lyrics.getContent());
+        assertNotNull(lyrics.getSource());
+        
+        lyrics = client.getLyrics("jklsjdgv89y32hr9").get();
+        assertNull(lyrics);
+        
+        lyrics = client.getLyrics("smooth criminal").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+        assertNotNull(lyrics.getAuthor());
+        assertNotNull(lyrics.getContent());
+        assertNotNull(lyrics.getSource());
+    }
+    
+    @Test
+    public void musixmatchTest() throws InterruptedException, ExecutionException
+    {
+        LyricsClient client = new LyricsClient("MusixMatch");
+        
+        Lyrics lyrics = client.getLyrics("ellie goulding lights").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+        assertNotNull(lyrics.getAuthor());
+        assertNotNull(lyrics.getContent());
+        assertNotNull(lyrics.getSource());
+        
+        lyrics = client.getLyrics("jklsjdgv89y32hr9").get();
+        assertNull(lyrics);
+        
+        lyrics = client.getLyrics("smooth criminal").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+        assertNotNull(lyrics.getAuthor());
+        assertNotNull(lyrics.getContent());
+        assertNotNull(lyrics.getSource());
+    }
+    
+    @Test
+    public void musicmatchTest() throws InterruptedException, ExecutionException
+    {
+        LyricsClient client = new LyricsClient("MusicMatch");
+        
+        Lyrics lyrics = client.getLyrics("smooth criminal").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+    }
+    
+    @Test
+    public void lyricsfreakTest() throws InterruptedException, ExecutionException
+    {
+        LyricsClient client = new LyricsClient("LyricsFreak");
+        
+        Lyrics lyrics = client.getLyrics("ellie goulding lights").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+        assertNotNull(lyrics.getAuthor());
+        assertNotNull(lyrics.getContent());
+        assertNotNull(lyrics.getSource());
+        
+        lyrics = client.getLyrics("jklsjdgv89y32hr9").get();
+        assertNull(lyrics);
+        
+        lyrics = client.getLyrics("smooth criminal").get();
+        assertNotNull(lyrics);
+        assertNotNull(lyrics.getTitle());
+        assertNotNull(lyrics.getAuthor());
+        assertNotNull(lyrics.getContent());
+        assertNotNull(lyrics.getSource());
+    }
+}
